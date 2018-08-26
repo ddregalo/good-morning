1e1549688629746a9f4f5a69ddba51843ad33eaf
danielcampos <ddregalo@gmail.com>
2018-08-27 00:01:10 +0100

    Test temperature display stub api call

--- tests/e2e/specs/components/Weather.Test.js
+++ tests/e2e/specs/components/Weather.Test.js
@@ -5,4 +5,16 @@ describe("Weather Component", () => {
     cy.visit("/");
     cy.contains("h3", "LOCAL WEATHER");
   });
+
+  it('Displays the temperature in Brixton from an api call', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://api.openweathermap.org/' +
+      'data/2.5/weather?q=Brixton,uk' + '&units=metric&APPID=' + Cypress.env("weatherAPIKey"),
+      response: {main: {temp: 25}}
+    })
+    cy.visit("/");
+    cy.get('h3').eq(1).contains('The temperature in Brixton right now is 25');
+  }); 
 });
