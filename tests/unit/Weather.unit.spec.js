52119c6d707168a96ce24f71f82d8c9526c98480
danielcampos <ddregalo@gmail.com>
2018-08-28 19:36:01 +0100

    Test temp display with failed api request

--- tests/unit/Weather.unit.spec.js
+++ tests/unit/Weather.unit.spec.js
@@ -16,4 +16,12 @@ describe("Weather Component Unit Tests", () => {
       'data/2.5/weather?q=Brixton,uk' + 
       '&units=metric&APPID=' + process.env.VUE_APP_OPEN_WEATHER_API_KEY);
   });
+
+  it('Returns the temperature as 0 if api call not successful', () => {
+    axios.get.mockClear();
+    const wrapper = shallowMount(Weather)
+    wrapper.vm
+    const brixtonTemp = wrapper.find('.weather-text')
+    expect(brixtonTemp.text()).toEqual('The temperature in Brixton right now is 0');
+  });
 });
